214565a66e893938635b6ca8a203e37cc44167d2
Andrearomga <211132@ids.upchiapas.edu.mx>
2023-10-29 18:45:04 -0600

    medio estaciona

--- views/vista.go
+++ views/vista.go
@@ -1,14 +1,14 @@
 package views
 
 import (
+	"simulador/models"
     "fyne.io/fyne/v2/app"
     "fyne.io/fyne/v2/canvas"
     "fyne.io/fyne/v2/container"
     "fyne.io/fyne/v2/widget"
     "fyne.io/fyne/v2/layout"
-    "fyne.io/fyne/v2"
-    "simulador/models"
-    "time"
+    "time" // Asegúrate de que esta línea esté presente para usar time
+	"fyne.io/fyne/v2"
 )
 
 func IniciarVentana() {
@@ -29,17 +29,24 @@ func IniciarVentana() {
         // Iniciar la simulación de llegada continua de vehículos desde el paquete models
         go models.SimularEstacionamiento(100, 20)
 
-        // Definir la posición inicial del carro
+        // Coordenadas iniciales del carro
         posX := float32(0)
         posY := float32(0)
 
-        // Crear una función para actualizar la posición del carro
-        actualizarPosicion := func() {
+        // Coordenadas de estacionamiento
+        cajonX := float32(130) // Coordenada X del cajón
+        cajonY := float32(70) // Coordenada Y del cajón
+
+        // Crear una función para mover el carro hacia el cajón
+        moverHaciaCajon := func() {
             for {
-                // Actualizar la posición del carro (simulación de movimiento)
-                posX += 2
-                if posX > float32(estacionamientoImagen.Size().Width) {
-                    posX = 0
+                if posX < cajonX {
+                    posX += 2
+                } else if posY < cajonY {
+                    posY += 2
+                } else {
+                    // El carro ha llegado al cajón
+                    break
                 }
 
                 // Actualizar la posición de la imagen del carro en la vista
@@ -49,8 +56,8 @@ func IniciarVentana() {
             }
         }
 
-        // Iniciar la función de actualización de posición en segundo plano
-        go actualizarPosicion()
+        // Iniciar la función de movimiento hacia el cajón en segundo plano
+        go moverHaciaCajon()
     })
 
     vistaConBoton := container.NewVBox(iniciarBoton, vista)
