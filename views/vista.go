0dbfdf49e85794c1c6e79491d5fcd9e3e351a72e
Andrearomga <211132@ids.upchiapas.edu.mx>
2023-11-11 20:59:11 -0600

    interfazzzzzz

--- views/vista.go
+++ views/vista.go
@@ -1,11 +1,13 @@
 package views
 
 import (
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
 
 func MostrarEstacionamiento() {
@@ -29,13 +31,24 @@ func MostrarEstacionamiento() {
 	carroImage.FillMode = canvas.ImageFillOriginal
 	carroImage.Resize(fyne.NewSize(60, 80)) // Ajusta el tamaño de la imagen del carro según tus necesidades
 
-	// Centrar la imagen del carro en el eje Y
-	windowHeight := myWindow.Canvas().Size().Height
-	carroY := (windowHeight - carroImage.Size().Height) / 2
+	// Obtener la altura total de los rectángulos
+	alturaTotal := float32(80 * len(rectangles))
+
+	// Posicionar la imagen del carro en la parte inferior
+	carroY := alturaTotal - 80 // 80 es la altura de un solo rectángulo
 	carroImage.Move(fyne.NewPos(0, carroY))
 
-	// Crear contenedor para los rectángulos y la imagen del carro
-	content := container.NewVBox(container.NewWithoutLayout(rectangles...), carroImage)
+	// Crear botón de inicio
+	iniciarButton := widget.NewButton("Iniciar", func() {
+		// Aquí puedes poner la lógica que se ejecutará cuando se presione el botón
+	})
+
+	content := container.NewVBox(
+		iniciarButton, // Mover el botón a la parte superior de la ventana
+		container.NewGridWrap(fyne.NewSize(60, 80), rectangles...),
+		layout.NewSpacer(),
+		carroImage,
+	)
 
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
